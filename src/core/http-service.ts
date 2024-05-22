d4aa9d7407b670912b78043d8c16087b879a5ff9
Erfun Ghodoosi <erfunghodoosi@gmail.com>
2024-05-22 16:20:32 +0330

    [config]: https service configuration

---
+++ src/core/http-service.ts
@@ -0,0 +1,84 @@
+// import { API_URL } from "@/configs/global";
+
+import axios, {
+    AxiosRequestConfig,
+    AxiosRequestHeaders,
+    AxiosResponse,
+} from "axios";
+
+const httpService = axios.create({
+    baseURL: /*API_URL*/ "https://test.com",
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+httpService.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        throw {
+            error,
+        };
+    },
+);
+
+async function apiBase<T>(
+    url: string,
+    options?: AxiosRequestConfig,
+): Promise<T> {
+    const response: AxiosResponse = await httpService(url, options);
+    return response.data as T;
+}
+
+async function readData<T>(
+    url: string,
+    headers?: AxiosRequestHeaders,
+): Promise<T> {
+    const options: AxiosRequestConfig = {
+        headers,
+        method: "GET",
+    };
+    return await apiBase<T>(url, options);
+}
+
+async function createData<TModel, TResult>(
+    url: string,
+    data: TModel,
+    headers?: AxiosRequestHeaders,
+): Promise<TResult> {
+    const options: AxiosRequestConfig = {
+        method: "POST",
+        headers,
+        data: JSON.stringify(data),
+    };
+
+    return await apiBase<TResult>(url, options);
+}
+
+async function updateData<TModel, TResult>(
+    url: string,
+    data: TModel,
+    headers?: AxiosRequestHeaders,
+): Promise<TResult> {
+    const options: AxiosRequestConfig = {
+        method: "PUT",
+        headers,
+        data: JSON.stringify(data),
+    };
+
+    return await apiBase<TResult>(url, options);
+}
+
+async function deleteData(
+    url: string,
+    headers?: AxiosRequestHeaders,
+): Promise<void> {
+    const options: AxiosRequestConfig = {
+        method: "DELETE",
+        headers,
+    };
+
+    return await apiBase(url, options);
+}
+
+export { createData, readData, updateData, deleteData };
