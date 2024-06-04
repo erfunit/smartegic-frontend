fb4a2c6a6f1530a28285d55cae002c8cfd2b8fd7
Erfun Ghodoosi <erfunghodoosi@gmail.com>
2024-06-04 11:29:03 +0330

    change registering form fields pattern

--- src/app/_components/form-generator/file-input-field/file-input-field.tsx
+++ src/app/_components/form-generator/file-input-field/file-input-field.tsx
@@ -3,12 +3,16 @@ import { FormFieldProps } from "@/types/input-fields.type";
 import React from "react";
 import FileInput from "../../file-input/file-input";
 
-export const FileInputField = ({ field, register, errors }: FormFieldProps) => (
-    <FileInput
-        variant="bordered"
-        acceptFormat={field.accept}
-        {...register(field.name, getRegisterOptions(field))}
-        placeholder={field.placeholder}
-        className={`${errors[field.name] ? "input-error" : ""}`}
-    />
-);
+export const FileInputField = ({ field, register, errors }: FormFieldProps) => {
+    const registerProps = register(field.name, getRegisterOptions(field));
+
+    return (
+        <FileInput
+            variant="bordered"
+            acceptFormat={field.accept}
+            placeholder={field.placeholder}
+            className={`${errors[field.name] ? "input-error" : ""}`}
+            {...registerProps}
+        />
+    );
+};
--- src/app/_components/form-generator/text-area-field/text-area-field.tsx
+++ src/app/_components/form-generator/text-area-field/text-area-field.tsx
@@ -3,13 +3,17 @@ import { FormFieldProps } from "@/types/input-fields.type";
 import { getRegisterOptions } from "@/lib/get-reg-options";
 import TextArea from "../../text-area/text-area";
 
-export const TextAreaField = ({ field, register, errors }: FormFieldProps) => (
-    <TextArea
-        {...register(field.name, getRegisterOptions(field))}
-        placeholder={field.placeholder}
-        rows={field.rows}
-        className={`textarea textarea-bordered ${
-            errors[field.name] ? "textarea-error" : ""
-        }`}
-    />
-);
+export const TextAreaField = ({ field, register, errors }: FormFieldProps) => {
+    const registerProps = register(field.name, getRegisterOptions(field));
+
+    return (
+        <TextArea
+            placeholder={field.placeholder}
+            rows={field.rows}
+            className={`textarea textarea-bordered ${
+                errors[field.name] ? "textarea-error" : ""
+            }`}
+            {...registerProps}
+        />
+    );
+};
