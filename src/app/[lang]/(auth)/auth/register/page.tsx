5a012a073f3dffc5aee6223aa3dea9e03529bcf8
Erfun Ghodoosi <erfunghodoosi@gmail.com>
2024-06-13 11:26:11 +0330

    style(register): correct styles and icons

---
+++ src/app/[lang]/(auth)/auth/register/page.tsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { AuthFormWrapper } from "../../_components/auth-form-wrapper";
+import { AuthFormTitle } from "../../_components/auth-form-title";
+import { AuthFormContainer } from "../../_components/auth-form-container";
+import { TextInput } from "@/app/_components/inputs/text-input";
+import { Label } from "@/app/_components/Typography/Label";
+import { Button } from "@/app/_components/button";
+import { CheckboxInput } from "@/app/_components/inputs/checkbox-input";
+import Link from "next/link";
+import Image from "next/image";
+import {
+    IconKey,
+    IconMail,
+    IconRegister,
+    IconUser,
+} from "@/app/_components/icons";
+
+const page = () => {
+    return (
+        <AuthFormWrapper>
+            <AuthFormTitle title="Register" />
+            <AuthFormContainer>
+                <form action="" className="space-y-4">
+                    <div className="w-full space-x-1">
+                        <Label>Username</Label>
+                        <TextInput
+                            name="username"
+                            type="text"
+                            placeholder="Username"
+                            startElement={<IconUser />}
+                        />
+                    </div>
+                    <div className="w-full space-x-1">
+                        <Label>Email Address</Label>
+                        <TextInput
+                            name="email"
+                            placeholder="Email Address"
+                            type="email"
+                            startElement={<IconMail />}
+                        />
+                    </div>
+                    <div className="w-full space-x-1">
+                        <Label>Password</Label>
+                        <TextInput
+                            name="password"
+                            placeholder="Password"
+                            type="password"
+                            startElement={<IconKey />}
+                        />
+                    </div>
+
+                    <div className="w-full flex items-center gap-2">
+                        <CheckboxInput variant="primary" id="accept" />
+                        <Label htmlFor="accept">
+                            I agree with{" "}
+                            <Link href="/" className="underline text-primary">
+                                terms and conditions
+                            </Link>
+                        </Label>
+                    </div>
+                    <Button
+                        variant="primary"
+                        shape="block"
+                        className="items-center flex"
+                    >
+                        <IconRegister />
+                        Register
+                    </Button>
+                    <Button
+                        variant="ghost"
+                        isOutline
+                        shape="block"
+                        className="flex items-center flex-row"
+                    >
+                        <Image
+                            src="/images/google.svg"
+                            width={26}
+                            height={26}
+                            alt="google"
+                        />
+                        Register With Google
+                    </Button>
+                </form>
+                <div className="flex mt-3">
+                    <span className="mx-auto text-sm">
+                        Already created one?{" "}
+                        <Link
+                            href="/auth/login"
+                            className="text-primary underline"
+                        >
+                            login
+                        </Link>
+                    </span>
+                </div>
+            </AuthFormContainer>
+        </AuthFormWrapper>
+    );
+};
+
+export default page;
--- src/app/_components/icons/src/user.tsx
+++ src/app/_components/icons/src/user.tsx
@@ -5,7 +5,7 @@ import { SvgIcon } from "@/app/_components/icons/icon.types";
 export default function SvgIconComponent(props:SvgIcon) {
   return (
     <BaseIcon {...props}>
-      <path d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75"/>
+      <path d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"/>
     </BaseIcon>
   );
 }
