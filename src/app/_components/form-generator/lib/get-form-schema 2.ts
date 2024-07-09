833a22bef445357691dd2840583c412cd6419d0a
Mohammad Erfun Ghodoosi <erfunghodoosi@gmail.com>
2024-07-09 16:50:36 +0330

    chore(duplication): remove duplicated files.

--- src/app/_components/form-generator/lib/get-form-schema 2.ts
+++
@@ -1,8 +0,0 @@
-import { FormSchema } from "@/types/form-generator.type";
-
-export const getFormSchema = async (): Promise<FormSchema> => {
-    const schema = import("../../../../../form_sample2.json").then(
-        (res) => res.data,
-    );
-    return schema as Promise<FormSchema>;
-};
